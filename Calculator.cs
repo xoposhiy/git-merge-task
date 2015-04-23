6408d9b592a910e992ff82d7f84d6a6fdaa076ee
Pavel Egorov <xoposhiy@mail.ru>
2015-04-23 20:41:00 +0500

    Calculator does not fail any more on error user input.

--- Calculator.cs
+++ Calculator.cs
@@ -9,7 +9,7 @@ namespace Kontur.Courses.Git
 			if (args.Length == 0)
 				return lastResult;
 			if (args.Length == 1)
-				return lastResult = double.Parse(args[0]);
+				return lastResult = TryParseDouble(args[0]);
 			if (args.Length == 2)
 			{
 				// Если не хватает первого аргумента, то использовать lastResult
@@ -18,17 +18,27 @@ namespace Kontur.Courses.Git
 				//> 4
 				// + 1
 				//>5
-				if (!lastResult.HasValue) return lastResult;
-				var v2 = double.Parse(args[1]);
-				return lastResult = Execute(args[0], lastResult.Value, v2);
+				var v1 = lastResult;
+				var v2 = TryParseDouble(args[1]);
+				if (!v1.HasValue || !v2.HasValue) return v1;
+				return lastResult = Execute(args[0], v1.Value, v2.Value);
 			}
 			if (args.Length == 3)
 			{
-				var v1 = double.Parse(args[0]);
-				var v2 = double.Parse(args[2]);
-				return lastResult = Execute(args[1], v1, v2);
+				var v1 = TryParseDouble(args[0]);
+				var v2 = TryParseDouble(args[2]);
+				if (!v1.HasValue || !v2.HasValue) return v1;
+				return lastResult = Execute(args[1], v1.Value, v2.Value);
 			}
 			return Maybe<double>.FromError("Error input");
+		}
+
+		private Maybe<double> TryParseDouble(string s)
+		{
+			double v;
+			if (double.TryParse(s, out v))
+				return v;
+			return Maybe<double>.FromError("Not a number '{0}'", s);
 		}
 
 		private Maybe<double> Execute(string op, double v1, double v2)
